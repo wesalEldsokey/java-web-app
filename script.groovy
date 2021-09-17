5e34da630ab9c4c133366ad654caef5341730deb
wesal Eldsokey <76208542+wesalEldsokey@users.noreply.github.com>
2021-09-17 15:58:53 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,15 @@
-def function() {
+def testApp() {
+   echo 'testing the application'
+   echo "Building version ${NEW_VERSION}"
+}
+
+def buildApp() {
   echo 'building the application '
 }
+
+
+def deployApp() {
+  echo 'deploying the application '
+}
+
 return this
